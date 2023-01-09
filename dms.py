9bdf1034417891c47b518c27bdab74db77237497
radiostatic0 <yayaroses102@yahoo.com>
2023-01-09 00:48:26 -0500

    check_for_new_dms() in dms.py will look up and send the corresponding Tweet when it sees a dm matching the regex requesting a specific frame. Also added the tweet logs from before the bot was logging each one as it is posted (NOTE: s1e3 and s1e4 are missing some tweets, have to re-scrape)

--- dms.py
+++ dms.py
@@ -15,11 +15,11 @@ api = tweepy.API(auth)
 def print_dm(dm):
     print(dm._json["id"], ': "', dm._json["message_create"]["message_data"]["text"], '"', sep="")
 
-#def check_for_new_dm():
-if True:
+def check_for_new_dm():
+#if True:
 
     old_dms=[]
-    if os.path.exists("dm_logs.txt"):
+    if os.path.exists("dm_log.txt"):
         dm_file=open("dm_log.txt") #a txt file containing the last 20 dms ids recieved (is cleared and rewritten consistently)
         old_dms = dm_file.readlines()
         dm_file.close()
@@ -52,12 +52,65 @@ if True:
         results = re.search(pattern, dm_text)
 
         if results is not None:
+            sender_id=int(dm._json["message_create"]["sender_id"])
+            
             season=int(results.group("s"))
             episode=int(results.group("e"))
             frame=int(results.group("f"))
 
             print(f"user wants to find s{season}e{episode} frame {frame}")
             #TODO: Call the function for responding.
-        
-        sender_id=int(dm._json["message_create"]["sender_id"])
-        
+
+            tweet_id=find_frame_id(season, episode, frame)
+
+            respond_find_frame(tweet_id, sender_id)
+
+
+def find_frame_id(season, episode, frame):
+
+    filename = f"id_logs/s{season}e{episode}_log.txt"
+    #print(filename)
+
+    if not os.path.exists(filename):
+        return -1
+
+    logfile=open(filename, "r")
+    lines=logfile.readlines()
+    logfile.close()
+
+    if frame-1>len(lines):
+        return -1
+
+    line=lines[frame-1]
+    print("Line:",line)
+
+    tweet_id=line.split(":")[1]
+
+    print("Tweet id:",tweet_id)
+
+    #print("Link: https://twitter.com/jbravo_frames/status/"+str(tweet_id))
+    return tweet_id
+
+def respond_find_frame(tweet_id, sender_id):
+    if tweet_id==-1:
+        message="Sorry, I couldn't find that frame (unspecified reason)"
+    else:
+        message=f"https://twitter.com/jbravo_frames/status/{tweet_id}"
+
+    api.send_direct_message(sender_id,message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
